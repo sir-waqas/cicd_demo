0e82f54dbde762c320ca0f12746fc2c6cdc68c98
Waqas Azam <waqas.azam@10pearls.com>
2019-07-02 17:53:08 +0500

    Demoing to Sadiq

--- test_script.sh
+++ test_script.sh
@@ -3,3 +3,4 @@ echo "This is from GitHub Public Repo -- A script to be executed by Jenkins"
 NUM=$(($FIRSTNUM+$SECONDNUM))
 echo "Adding Two Parameters Give: $NUM"
 echo "This is a test change which tests the job on git push and webhook on jenkins"
+echo "Demoing this to Sadiq"
